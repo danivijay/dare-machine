2703234cfa05acfbf43ddbd0819c68cb8c089095
danimvijay <danimvijay@gmail.com>
2017-12-10 11:56:53 +0530

    environment setup

---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+var daremachine = require('./daremachine.js');
---
+++ assets/js/daremachine.js
@@ -0,0 +1,4 @@
+define(||, function(){
+	//var apiUrlPath = "https://danivijay.com/dare-machine/service/";
+	//var apiUrlLatest = apiUrlPath + 'fetch-dataset.php';
+})

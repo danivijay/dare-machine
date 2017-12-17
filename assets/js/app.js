73e106c631ef891f5ac41e3e3c39e0b8f1b9ca81
Dani Vijay <danimvijay@gmail.com>
2017-12-17 19:36:42 +0530

    debugging define is not defined

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,2 @@
 var daremachine = require('./daremachine.js');
-// var express = require('express');
-// var path = require('path');
-// var open = require('open');
-// var port = 3000; //choose different, if this port not available
-// var app = express();
-// app.get('/',function(req, res) {
-// res.sendFile(path.join(__dirname, '../index.html'));
-// });
-// app.listen(port, function(err) {
-// if (err) {
-// console.log(err);
-// } else {
-// open('http://localhost' + port);
-// }
-// });
-
 daremachine.loadMoreRequest();
